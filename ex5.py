381516ab1782113a130f46a03b65facdd96d581b
22p21n0056-chan <60224796+channo39mz@users.noreply.github.com>
2024-12-01 20:13:38 +0700

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,11 @@
+text = "This is the house that Jack built."
+
+print(text[:4])
+print(text[5:7])
+print(text.find("This"))
+print(text.find("J"))
+print(text.find("is", 4))
+print(len(text))
+text2 = text[5:]
+print(text2)
+print(len(text2))
